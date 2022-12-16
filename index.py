5cf3f6b3df94e232f56b97238f5ce75eba401383
David Zhang <xiduo2004@hotmail.com>
2022-12-15 19:52:09 -0500

    just commit them

--- index.py
+++ index.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from flask import Flask, request
+from flask import Flask, request, redirect, url_for
 from flask import render_template
 from flask import g
 from .database import Database
@@ -45,15 +45,42 @@ def need_adopt():
     return render_template('adoption.html')
 
 @app.route('/animal/<id>')
-def animal_id(id):
+def animal_with_id(id):
     animal = get_db().get_animal(id)
     return render_template('animalshow.html', animal = animal)
 
-@app.route('/animal/search/<id>')
-def search(id):
-    animal = get_db().get_animal(id)
-    return render_template('animalshow.html', animal = animal)
+@app.route('/search')
+def search():
+    query = request.args.get('query').lower()
+    animals = get_db().get_animaux()
+    filter_animals = _filter_animals(animals, query)
+    return render_template('result.html', animals = filter_animals)
+
+@app.route("/submit", methods=["post"])
+def submit():
+    name = request.form["animal_name"]
+    specie = request.form["animal_specie"]
+    race = request.form["animal_race"]
+    age = request.form["animal_age"]
+    email = request.form["email"]
+    addresse = request.form["addresse"]
+    city = request.form["city"]
+    postal = request.form["postal"]
+    description = request.form["description"]
+
+    animal_id = get_db().add_animal(name, specie, race, age, description, email, addresse, city, postal)
+
+    return redirect(url_for('animal_with_id', id=animal_id))
 
 @app.errorhandler(404)
 def not_found(e):
     return render_template("404.html"), 404
+
+
+def _filter_animals(animals, query):
+    filter_animals = []
+    for animal in animals:
+        if animal['espece'].lower() in query or animal['nom'].lower() in query:
+            filter_animals.append(animal)
+
+    return filter_animals
--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,5 @@
 // L'endroit où placer le code du front-end.
-function searchForm() {
+function submitForm() {
   if (formValidation()) {
     document.forms["formulaire"].submit();
   } else {
@@ -7,6 +7,119 @@ function searchForm() {
   }
 }
 
+function formValidation() {
+  clearAllErrorMessage();
+  let validate = formElementsValidation();
+
+  return validate;
+}
+
+function formElementsValidation() {
+  const fieldEmptyMessage = "Le champ ne doit pas être vide.";
+  const emailFormatMessage = "Le courriel n'est pas du bon format.";
+  const postalFormatMessage = "Le code postal n'est pas du bon format.";
+  const commaFormatMessage = "Ne peut pas contenir de virgule";
+  const ageMessage = "L'age doit être 0 et 30";
+  const nameMessage = "Le nom de l'animal doit avoir entre 3 et 20 caractères";
+
+  let validated = true;
+  let emailRegx = new RegExp(/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/, "gm");
+  let postalRegx = new RegExp(/^[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d$/)
+
+  let commaEx = new RegExp('^[^,]+$');
+  let form = document.forms["formulaire"];
+
+
+  let animalName = form["animal_name"].value;
+  let specieVal = form["animal_specie"].value;
+  let raceVal = form["animal_race"].value;
+  let ageVal = form["animal_age"].value;
+  let emailVal = form["email"].value;
+  let addresseVal = form["addresse"].value;
+  let cityVal = form["city"].value;
+  let postalVal = form["postal"].value;
+  let descriptionVal = form["description"].value;
+
+
+
+
+  // animal name validation
+  if(animalName == "") {
+    document.getElementById("animal-name-err").innerHTML = fieldEmptyMessage;
+    validated = false;
+  } else if (animalName.length < 3 || animalName.length > 20) {
+    document.getElementById("animal-name-err").innerHTML = nameMessage;
+    validated = false
+  }
+
+  // specie validation
+  if (specieVal == "") {
+    document.getElementById("specie-err").innerHTML = fieldEmptyMessage;
+    validated = false;
+  }
+
+  // race validation
+  if (raceVal == "") {
+    document.getElementById("race-err").innerHTML = fieldEmptyMessage;
+    validated = false;
+  }
+
+  // age validation
+  if (ageVal == "") {
+    document.getElementById("age-err").innerHTML = fieldEmptyMessage;
+    validated = false;
+  } else if (ageVal < 0 || ageVal > 30) {
+    document.getElementById("age-err").innerHTML = ageMessage;
+    validated = false;
+  }
+
+  // email validation
+  if(emailVal == "") {
+    document.getElementById("email-err").innerHTML = fieldEmptyMessage;
+    validated = false;
+  }else if (!emailRegx.test(emailVal)) {
+    document.getElementById("email-err").innerHTML = nameFormatMessage;
+    validated = false;
+  }
+
+  // addresse validation
+  if (addresseVal == "") {
+    document.getElementById("addresse-err").innerHTML = fieldEmptyMessage;
+    validated = false;
+  }
+  // city validation
+  if (cityVal == "") {
+    document.getElementById("city-err").innerHTML = fieldEmptyMessage;
+    validated = false;
+  }
+
+  // postal code validation
+  if (postalVal == "") {
+    document.getElementById("postal-err").innerHTML = fieldEmptyMessage;
+    validated = false;
+  } else if (!postalRegx.test(postalVal)) {
+    document.getElementById("postal-err").innerHTML = postalFormatMessage;
+    validated = false
+  }
+
+  // description validation
+  if (descriptionVal == "") {
+    document.getElementById("description-err").innerHTML = fieldEmptyMessage;
+    validated = false;
+  }
+
+  return validated;
+}
+
+function clearAllErrorMessage() {
+  let errorSpans = [...document.getElementsByClassName("error")];
+
+  errorSpans.forEach((span) => {
+    span.innerHTML = " ";
+  });
+}
+
 function handleErrorImg(e) {
   e.src = "/images/coming.jpeg"
 }
+
